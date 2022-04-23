19ea6aae7ae18994002dfa37c7138bc11e9964af
Bhagyabhavi <103823947+Bhagyabhavi@users.noreply.github.com>
2022-04-24 00:02:27 +0530

    assignment

---
+++ area_of_rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int l,b,area=0;
+printf("enter the value of l");
+scanf("%d",l);
+printf("enter the value of b");
+scanf("%d",b);
+area=l*b;
+printf("the are of rectangle is",area);
+getch();
+}
