e431d333cc0d93cc2618a508e158eeaccc5ba02e
Bhagyabhavi <103823947+Bhagyabhavi@users.noreply.github.com>
2022-04-24 09:20:49 +0530

    assignment

---
+++ area_of_circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int r;
+ double area=0.0;
+ printf("enter the radius of a circle");
+ scanf("%d",r);
+ area=(22/7)*r*r;
+ printf("the area of a circle is",area);
+ getch();
+}
