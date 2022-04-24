60a1a7efbb86d361ef19187bbe2d8bd27b243645
Bhagyabhavi <103823947+Bhagyabhavi@users.noreply.github.com>
2022-04-24 09:22:16 +0530

    assignment

---
+++ parameter_of_rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,p=0;
+printf("enter the value of l");
+scanf("%d",a);
+printf("enter the value of b");
+scanf("%d",b);
+p=2(l+b);
+printf("the parameter ofrectangle is",p);
+getch();
+}
