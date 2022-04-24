61e1cbff55396b01bee27b3d97376dbef11b32fb
Bhagyabhavi <103823947+Bhagyabhavi@users.noreply.github.com>
2022-04-24 09:26:15 +0530

    assignment

---
+++ simple_interest.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int p,t;
+ double r,SI=0.0;
+printf("enter the value of p");
+scanf("%d",p);
+printf("enter the value of r");
+scanf("%d",r);
+printf("enter the value of t");
+scanf("%d",t);
+SI=(p*r*t)/100;
+printf("the simple interest is",SI);
+getch();
+}
